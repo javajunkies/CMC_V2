479fb3c7109fdeec5690497321d8e11a352804b0
DKUHR001 <dkuhr001@vplinphys23.ad.csbsju.edu>
2019-03-12 21:32:14 -0500

    DB Controller with new build config

--- Phase2Driver.java
+++ Phase2Driver.java
@@ -3,18 +3,20 @@
 
 public class Phase2Driver{
   public void main(){
-    
+   DBController db=new DBController();
+   
+   String[][] schools=db.getAllUniversities();
+   System.out.println(schools);
+   /**
    Scanner sc=new Scanner(System.in);
    System.out.println("Enter username: ");
    String username=sc.nextLine();
    System.out.println("Enter password: ");
    String password=sc.nextLine();
-   /**help here Imad login(username,password)
-   **/
    String username= User.getUsername();
    String first=
    (username,first,last,password,type,status);
-  
+   **/
   
   }  
 }
