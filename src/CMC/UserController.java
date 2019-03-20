5f29baad580968c5241987ad866bd5fa603c9880
JWOLFF001 <jwolff001@vplincsci018.ad.csbsju.edu>
2019-03-19 21:22:56 -0500

    updates

--- src/CMC/UserController.java
+++ src/CMC/UserController.java
@@ -43,12 +43,40 @@ public class UserController {
  }
 
  /**
-  * Searches for schools matching the entered criteria
-  * @param schools a list containing desired search criteria
+  * This method calls the DBController to complete the search algorithm
+  * 
+  * @param mySchool  mySchool is the name of the school
+  * @param myState   is the state that the school is located
+  * @param myLocation is the type of area in which the school is located (i.e. Suburban, Small-city, ...)
+  * @param myControl specifies who controls the university (i.e. Public, Private, ...)
+  * @param minNumStudents is the min number of students desired at a university
+  * @param maxNumStudents is the max number of students desired at a university
+  * @param minPercentFemale is the min percentage of females desired at a university
+  * @param maxPercentFemale is the max percentage of females desired at a university
+  * @param minSATVerbal is the min average SAT Verbal score, out of 800, for the students at a university
+  * @param maxSATVerbal is the max average SAT Verbal score, out of 800, for the students at a university
+  * @param minSATMath is the min average SAT Math score, out of 800, for the students at a university
+  * @param maxSATMath is the max average SAT Math score, out of 800, for the students at a university
+  * @param minExpenses is the min annual tuition for a university
+  * @param maxExpenses is the max annual tuition for a university
+  * @param minPercentFinancialAid is the min percentage of students who received some sort of financial aid from a university
+  * @param maxPercentFinancialAid is the max percentage of students who received some sort of financial aid from a university
+  * @param minNumApplicants is the min number of students who typically apply to this university
+  * @param maxNumApplicants is the max number of students who typically apply to this university
+  * @param minPercentAdmitted is the min percentage of students who are admitted annually to a university
+  * @param maxPercentAdmitted is the max percentage of students who are admitted annually to a university
+  * @param minPercentEnrolled is the min percentage of students who are currently enrolled at this university
+  * @param maxPercentEnrolled is the max percentage of students who are currently enrolled at this university
+  * @param minAcademicsScale is an integer between 1 and 5 (5 being the best) indicating the min quality of academics at this university 
+  * @param maxAcademicsScale is an integer between 1 and 5 (5 being the best) indicating the max quality of academics at this university 
+  * @param minSocialScale is an integer between 1 and 5 (5 being the best) indicating the min quality of the social life at this university
+  * @param maxSocialScale is an integer between 1 and 5 (5 being the best) indicating the max quality of the social life at this university
+  * @param minQualityOfLife is an integer between 1 and 5 (5 being the best) indicating the min overall quality of life at this university
+  * @param maxQualityOfLife is an integer between 1 and 5 (5 being the best) indicating the max overall quality of life at this university
   */
- public void search(List<String> schools) {
-  dbcontroller.searchUniversities(schools);
-  ////???????????
+ public void search(String mySchool,String myState,String myLocation,String myControl,int minNumStudents, int maxNumStudents,double minPercentFemale, double maxPercentFemale,double minSATVerbal, double maxSATVerbal,double minSATMath, double maxSATMath,double minExpenses, double maxExpenses,double minPercentFinancialAid, double maxPercentFinancialAid,int minNumApplicants, int maxNumApplicants, double minPercentAdmitted, double maxPercentAdmitted,double minPercentEnrolled, double maxPercentEnrolled,int minAcademicsScale, int maxAcademicsScale,int minSocialScale, int maxSocialScale,int minQualityOfLife, int maxQualityOfLife)
+ {
+  dbcontroller.searchUniversities(mySchool, myState, myLocation, myControl, minNumStudents, maxNumStudents, minPercentFemale, maxPercentFemale, minSATVerbal, maxSATVerbal, minSATMath, maxSATMath, minExpenses, maxExpenses, minPercentFinancialAid, maxPercentFinancialAid, minNumApplicants, maxNumApplicants, minPercentAdmitted, maxPercentAdmitted, minPercentEnrolled, maxPercentEnrolled, minAcademicsScale, maxAcademicsScale, minSocialScale, maxSocialScale, minQualityOfLife, maxQualityOfLife);
  }
  
  /**
