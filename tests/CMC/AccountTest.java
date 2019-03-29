481fd570ed9b2a762711c19d943007b756c31ac0
TROBINSON001 <trobinson001@vplincsci089.ad.csbsju.edu>
2019-03-29 15:45:44 -0500

    tests

---
+++ tests/CMC/AccountTest.java
@@ -0,0 +1,82 @@
+package CMC;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AccountTest {
+
+	private Account a;
+	
+	@Before
+	public void setUp() throws Exception {
+		a = new User("Test", "Name", "accountTest", "Password1", 'a', 'Y');
+	}
+
+	@Test
+	public void testAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFirst() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFirst() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLast() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLast() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUsertype() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUsertype() {
+		fail("Not yet implemented");
+	}
+
+}
