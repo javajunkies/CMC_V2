b516c604529fa5f291edc702340d47c43e7cde6a
JBRAKOB001 <jbrakob001@vplincsci026.ad.csbsju.edu>
2019-04-01 13:31:11 -0500

    dbcontroller tests

---
+++ tests/CMC/DBControllerTest.java
@@ -0,0 +1,31 @@
+package CMC;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DBControllerTest {
+
+	private DBController db;
+	
+	@Before
+	public void setUp() throws Exception {
+		db = new DBController();
+	}
+	
+	//isUniqueUsername tests
+	@Test
+	public void isUniqueUsernameTestFalse() {
+		Boolean isUnique = db.isUniqueUsername("nadmin");
+		assertEquals(isUnique, false);
+	}
+	
+	@Test
+	public void isUniqueUsernameTestTrue() {
+		Boolean isUnique = db.isUniqueUsername("NewUsername");
+		assertEquals(isUnique, true);
+	}
+
+}
