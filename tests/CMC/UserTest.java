e0d7c44d49167f19d407464f50bdebcc31e614eb
JBRAKOB001 <jbrakob001@vplincsci072.ad.csbsju.edu>
2019-04-02 01:01:44 -0500

    tests for db and User

---
+++ tests/CMC/UserTest.java
@@ -0,0 +1,25 @@
+package CMC;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+	private User u;
+	
+	@Before
+	public void setUp() throws Exception {
+		u = new User("Test", "Name", "accountTest", "Password1", 'a', 'Y');
+	}
+
+	@Test
+	public void ToStringtest() {
+		String ToStringTest = "User " + "Test" + "\n" + "Name" + "\n" + "accountTest" + "\n" + "Password1" + "\n" + 'a' + "\n" + 'Y' + "\n" ;
+		String expected = u.toString();
+		assertEquals(expected, ToStringTest);
+	}
+
+}
