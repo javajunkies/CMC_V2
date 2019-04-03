f50fc06c12c6fffa791c343b22c853ac2736b25b
BWEST001 <bwest001@vplincsci053.ad.csbsju.edu>
2019-04-02 20:26:51 -0500

    commit

---
+++ tests/CMC/AccountControllerTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package CMC;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author bwest001
+ *
+ */
+public class AccountControllerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
