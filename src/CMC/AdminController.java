d73fd2a82c8034a28ccc774a93adf809d6357a59
TROBINSON001 <trobinson001@vplincsci073.ad.csbsju.edu>
2019-04-09 13:34:57 -0500

    fixed add new user

--- src/CMC/AdminController.java
+++ src/CMC/AdminController.java
@@ -394,11 +394,11 @@ public class AdminController {
 		  throw new IllegalArgumentException("Invalid password, must have atleast 1 number.");
 		  //return -6;
 	  }
-	  else if(type != 'u' || type != 'U' || type != 'a' || type != 'A' || type != 't' || type != 'T') {
-		  throw new IllegalArgumentException("Invalid account type.");
+	  else if(type == 'u' || type == 'U' || type == 'a' || type == 'A' || type == 't' || type == 'T') {
+		  return dbcontroller.createUser(firstName, lastName, username, password, type);
 	  }
 	  else {
-		  return dbcontroller.createUser(firstName, lastName, username, password, type);
+		  throw new IllegalArgumentException("Invalid account type.");
 	  }
 	  
   }
