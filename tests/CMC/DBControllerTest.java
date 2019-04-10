823f653fa4b2029cc63488aaa1d0c43224281d05
JBRAKOB001 <jbrakob001@vplincsci038.ad.csbsju.edu>
2019-04-09 19:45:21 -0500

    edits

--- tests/CMC/DBControllerTest.java
+++ tests/CMC/DBControllerTest.java
@@ -60,7 +60,7 @@ public class DBControllerTest {
 		University Uni = new University("UNIVERSITY OF CALIFORNIA BERKELEY", "CALIFORNIA", "URBAN", "STATE", 40000, 45.0, 530.0, 600.0, 15328.0, -1.0, 15000, 50.0, 70.0, 5, 3, 3);
 		expected.add(Uni);
 		ArrayList<University> searchTest = db.searchUniversities("ber", "calif", false, "urb", "st", 0 ,40000, 0.0, 45.0, 0.0, 530.0, 0.0, 600.0, 0.0, 15328.0, 0.0, 0.0, 0, 15000, 0.0, 50.0, 0.0, 70.0, 0, 5, 0, 3, 0, 3);
-		//ArrayList<University> searchTest = db.searchUniversities("", "", false, "", "",0,0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0,0,0.0,0.0,0.0,0.0,0,0,0,0,0,0);
+		//ArrayList<University> searchTest = db.searchUniversities("A", "", false, "", "",0,0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0,0,0.0,0.0,0.0,0.0,0,0,0,0,0,0);
 		
 //		for (int i = 0; i!= searchTest.size();i++)
 //		{
@@ -94,8 +94,17 @@ public class DBControllerTest {
 		assertTrue("Should return " + expected + " but returned " + actual, expected == actual);
 	}
 	
-	@Test
-	
+//	@Test
+//	public void getRecommendationsTest() {
+//		University Uni = new University("UNIVERSITY OF CALIFORNIA BERKELEY", "CALIFORNIA", "URBAN", "STATE", 40000, 45.0, 530.0, 600.0, 15328.0, -1.0, 15000, 50.0, 70.0, 5, 3, 3);
+//		ArrayList<University> Test = db.getRecommendations(Uni);
+//		
+//		for (int i = 0; i!= Test.size();i++)
+//		{
+//			System.out.println(Test.get(i));
+//		}
+//		
+//	}
 
 	@After
 	public void after()
