bb93e45c67f58e4762b7637e8178dbb98a95bba8
TROBINSON001 <trobinson001@vplincsci063.ad.csbsju.edu>
2019-04-09 20:42:10 -0500

    UseCaseTesting

--- tests/CMC/UseCaseTesting.java
+++ tests/CMC/UseCaseTesting.java
@@ -32,5 +32,128 @@ public class UseCaseTesting {
 		int actual = ui.login("testUser", "Password1");
 		assertTrue("Login returned " + actual + " it should have returned " + expected ,expected == actual);
 	}
+	
+	@Test
+	public void testU1LoginA1() {
+		int expected = 1;
+		int actual = ui.login("FakeUsername", "Password1");
+		assertEquals("Login should return " + expected, expected, actual);
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		int expected = 2;
+		int actual = ui.login("testUser", "FakePassword");
+		assertEquals("Login should return " + expected, expected, actual);
+	}
+	
+	@Test
+	public void testU1LoginA3() {
+		int expected = 3;
+		int actual = ui.login("luser", "user");
+		assertEquals("Login should return " + expected, expected, actual);
+	}
+
+	@Test
+	public void testU1LoginA4() {
+		ui.register("Tyreese", "Robinson", "testTempAccount", "Password1", "Password1");
+		int expected = 4;
+		int actual = ui.login("testTempAccount", "Password1");
+		assertEquals("Login should return " + expected, expected, actual);
+		ai.deleteUser("testTempAccount");
+	}
+	
+	@Test
+	public void testU2Register() {
+		int expected = 0;
+		int actual = ui.register("Tyreese", "Robinson", "testRegister", "Password1", "Password1");
+		assertEquals(expected, actual);
+		db.deleteUser("testRegister");
+	}
+	
+	@Test
+	public void testU2RegisterA1() {
+		
+	}
+	
+	@Test
+	public void testU2RegisterA2() {
+		
+	}
+	
+	@Test
+	public void testU2RegisterA3() {
+		
+	}
+/*	
+	@Test
+	public void testU1LoginA2() {
+		
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		
+	}
+	
+	@Test
+	public void testU1LoginA2() {
+		
+	}*/
 
 }
